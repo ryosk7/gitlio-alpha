ee7dd01bba21db252faa1ca03bf56f9d86bf8180
Ryosuke Uchida <uchiryo7@gmail.com>
2018-04-13 00:47:33 +0900

    github-omniauthを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  
+  get 'users/index'
+
+  get 'auth/:provider/callback', to: 'sessions#callback'
+  get '/logout', to: 'sessions#destroy'
+
+  root 'homes#top'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
